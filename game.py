9ad991e6a0963d7b37625a9651dd0718a2be0f01
wbslsy <3433848894@qq.com>
2020-12-23 00:45:17 +0800

    21

--- game.py
+++ game.py
@@ -7,6 +7,10 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alien Invasion")
+
+    #设置背景色
+    bg_color = (230,230,230)
+
     #开始游戏主循环
     while True:
 
@@ -14,6 +18,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        #每次循环重绘屏幕
+        screen.fill(bg_color)        
 
         #让最近绘制的屏幕可见
         pygame.display.flip()
