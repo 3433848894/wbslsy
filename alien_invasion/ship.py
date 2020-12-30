7a11db472a32071963ab0da4e848b0847906bb6f
wbslsy <3433848894@qq.com>
2020-12-30 14:36:48 +0800

    1

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -7,7 +7,7 @@ class Ship():
         self.screen = screen
 
         #加载飞船图像并获得其外接矩形
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/sad.jpg')
         self.rect = self.image.pygame.event.get_rect()
         self.screen_rect = screen.get_rect()
 
