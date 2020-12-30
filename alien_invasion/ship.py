8ab17324c7b18f43034e99025d7e365dae9dd72f
wbslsy <3433848894@qq.com>
2020-12-30 14:35:06 +0800

    432

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -2,12 +2,19 @@ import pygame
 
 class Ship():
 
-    def_init_(self,screen):
-    """初始化飞船并设置初始位置"""
-    self.screen = screen
+    def __init__(self,screen):
+        """初始化飞船并设置初始位置"""
+        self.screen = screen
 
-    #加载飞船图像并获得其外接矩形
-    self.image = pygame.image.load('images/ship.bmp')
-    self.rect = self.image.pygame.event.get_rect()
-    self.screen_rect = screen.get_rect()
+        #加载飞船图像并获得其外接矩形
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.pygame.event.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        #将每个图像放在屏幕底部中央
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottoom = self.screen_rect.bottom
     
+    def blitme(self):
+        """在指定位置绘制图像"""
+        self.screen.blit(self.image,self.rect)
