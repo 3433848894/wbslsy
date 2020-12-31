2587f6338e491556dd79f5f59bc18d4274785c99
wbslsy <3433848894@qq.com>
2020-12-31 12:16:57 +0800

    1

--- alien_invasion/game.py
+++ alien_invasion/game.py
@@ -12,7 +12,7 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #开始游戏主循环
+    #开始主循环
     while True:
 
         #监视键盘和鼠标事件
