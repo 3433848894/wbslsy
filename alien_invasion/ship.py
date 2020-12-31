d9121cc8b1a883860e8b9b2e61eaac046963462f
wbslsy <3433848894@qq.com>
2020-12-31 12:02:29 +0800

    543

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -3,10 +3,10 @@ import pygame
 class Ship():
 
     def __init__(self,screen):
-        """初始化飞船并设置初始位置"""
+        """初始化图像并设置初始位置"""
         self.screen = screen
 
-        #加载飞船图像并获得其外接矩形
+        #加载图像并获得其外接矩形
         self.image = pygame.image.load('images/sad.jpg')
         self.rect = self.image.pygame.event.get_rect()
         self.screen_rect = screen.get_rect()
