6462bdc1e89a3b6c7e229d8edf59a3a9da36fb15
wbslsy <3433848894@qq.com>
2021-01-04 18:37:46 +0800

    1

--- alien_invasion/game.py
+++ alien_invasion/game.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     #初始化pygame,设置和屏幕对象
@@ -18,11 +19,7 @@ def run_game():
 
     #开始主循环
     while True:
-
-        #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
         #每次循环重绘屏幕
         screen.fill(ai_settings.bg_color)
---
+++ alien_invasion/game_functions.py
@@ -0,0 +1,9 @@
+import sys
+
+import pygame
+
+def check_events():
+    """响应按键和鼠标事件"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
