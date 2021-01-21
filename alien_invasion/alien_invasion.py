a4c7f4429f1c3ec62e7470a9f7ae9de1c3a9ab03
wbslsy <3433848894@qq.com>
2021-01-21 19:30:58 +0800

    2

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,7 +1,7 @@
 import sys
 
 import pygame
-
+from pygame.sprite import Group 
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -16,12 +16,15 @@ def run_game():
 
     #创建一个图像
     ship = Ship(ai_settings,screen)
+    #创建一个用于存储子弹的编组
+    bullets = Group()
 
     #开始主循环
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        bullets.update()
+        gf.update_screen(ai_settings,screen,ship,bullets)
 
 run_game()
                 
--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -19,3 +19,14 @@ class Bullet(Sprite):
 
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        """向上移动子弹"""
+        #更新表示子弹位置的小数值
+        self.y -= self.speed_factor
+        #更新表示子弹位置的rect的位置
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """在屏幕上绘制子弹"""
+        pygame.draw.rect(self.screen,self.color,self.rect)
