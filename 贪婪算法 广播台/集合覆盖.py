904a0d03760f985e2efd3656fcb7047c31eef5aa
wbslsy <3433848894@qq.com>
2021-03-19 17:44:29 +0800

    1

--- 贪婪算法 广播台/集合覆盖.py
+++ 贪婪算法 广播台/集合覆盖.py
@@ -13,11 +13,19 @@ stations["kfive"] = set(["ca","az"])
 final_stations = set()
 
 #遍历所有的广播台，选择覆盖了最多未覆盖州的广播台，存储在best_station中
-best_station = None
-states_covered = set()
-for station,states_for_station in stations.items():
+while states_needed:
+    best_station = None
+    states_covered = set()
+    for station,states_for_station in stations.items():
 #states_covered是一个集合，包含该广播台覆盖的所有未覆盖的州。for循环迭代每个广播台，并确定它是否最佳
-covered = srates_needed & states_for_station
-if len()covered > len(states_covered):
-    best_station = station
-    states_covered = covered
+        covered = states_needed & states_for_station
+#检查该广播台覆盖的州是否比best_station多
+        if len(covered) > len(states_covered):
+            best_station = station
+            states_covered = covered
+#在for循环结束后将best_station添加到最终的广播台列表中
+final_stations.add(best_station)
+#更新states_needed,该广播台覆盖了一些州，因此不用覆盖这些州
+states_needed -= states_covered
+#最后打印final_stations
+print (final_stations)
