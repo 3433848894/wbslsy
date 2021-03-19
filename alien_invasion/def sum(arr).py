a58e14638ef2d4dceed95d0bfb950a6506e7472e
wbslsy <3433848894@qq.com>
2021-03-19 14:56:51 +0800

    yes

---
+++ alien_invasion/def sum(arr).py
@@ -0,0 +1,6 @@
+def sum(arr):
+    if sum == []:
+        return 0
+    return 1 + sum(arr[1:])
+
+sum(1,2,3)
--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -3,7 +3,7 @@ import sys
 import pygame
 from bullet import Bullet
 
-def check_keydown_events(event,ai_settings,screen,ship,alien,bullets):
+def check_keydown_events(event,ai_settings,screen,ship,bullets):
     """响应按键"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
@@ -40,7 +40,7 @@ def check_events(ai_settings,screen,ship,bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
               
-def update_screen(ai_settings,screen,ship,bullets):
+def update_screen(ai_settings,screen,ship,alien,bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
     #每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
