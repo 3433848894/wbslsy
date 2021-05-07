2ab96998c3152b9748cb386b780311d7361c7aab
wbslsy <3433848894@qq.com>
2021-05-07 17:33:59 +0800

    1

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -63,20 +63,26 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+def get_number_aliens_x(ai_settings,alien_width):
+    """计算每行可容纳多少个外星人"""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+def create_alien(ai_settings,screen,aliens,alien_number):
+    """创建一个外星人并将其加入当前行"""
+    alien = Alien(ai_settings,screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
 
 def create_fleet(ai_settings,screen,aliens):
     """创建外星人群"""
     #创建一个外星人，并计算一行可容纳多少个外星人
-    #外星人间距为外星人宽度
     alien = Alien(ai_settings,screen)
-    alien_width = alien.rect.width
-    available_space_x = ai_settings.screen_width - 2 * alien_width
-    number_aliens_x = int(available_space_x / (2 * alien_width))
+    number_aliens_x = get_number_aliens_x(ai_settings,alien.rect.width)
 
     #创建第一行外星人
     for alien_number in range(number_aliens_x):
-        #创建第一个外星人并将其加入当前行
-        alien = Alien(ai_settings,screen)
-        alien.x = alien_width + 2 * alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+        create_alien(ai_settings,screen,aliens,alien_number)
