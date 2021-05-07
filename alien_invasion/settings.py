fd767e7151f306290ecae6c9b3327706a8728cb6
wbslsy <3433848894@qq.com>
2021-05-07 16:43:42 +0800

    1

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -9,11 +9,11 @@ class Settings():
         self.bg_color = (230,230,230)
 
         #飞船的设置
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 1
 
         #子弹设置
         self.bullet_speed_factor = 0.2
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60,60,60
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
