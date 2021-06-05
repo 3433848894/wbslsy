b8a1e18d2bc14adc14a92aad226a7a4231fa5bd4
wbslsy <3433848894@qq.com>
2021-06-05 22:18:22 +0800

    1

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -28,7 +28,8 @@ def run_game():
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.update_bullets(bullets)        
+        gf.update_bullets(bullets)
+        gf.update_aliens(aliens)        
         gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -17,3 +17,6 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = 60,60,60
         self.bullets_allowed = 5
+
+        #外星人设置
+        self.alien_speed_factor = 1
