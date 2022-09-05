e138e486b171c076a0d49344374fa7d10d82bcf1
wbslsy <3433848894@qq.com>
2022-09-05 15:29:47 +0800

    1

---
+++ java/public class helloworld {.java
@@ -0,0 +1,16 @@
+public class helloworld {
+    public static void main(String[] args) {
+        
+    
+        int a=1;
+        int b=2;
+        int c=3;
+        if (a>b && a>c)
+            System.out.println(a);
+        if (b>a && b>c)
+            System.out.println(b);
+        if (c>a && c>b)
+            System.out.println(c);
+
+    }
+}
